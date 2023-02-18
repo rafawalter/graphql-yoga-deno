8af922101515b90c1ab6428fd487a0995bf612da
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2023-02-18 20:28:13 -0300

    extract typedefs and resolvers

---
+++ typedefsAndResolvers.ts
@@ -0,0 +1,31 @@
+import {throwErrorGraphql, throwErrorMasked} from "./resolvers/throw-error.resolver.ts";
+import {deferFastField, deferSlowField} from "./resolvers/deferAndStream.resolver.ts";
+import {countdown} from "./resolvers/countdown.resolver.ts";
+
+export const typeDefs = /* GraphQL */ `
+    type Query {
+        hello: String!
+        throwErrorGraphql: String!
+        throwErrorMasked: String!
+        alphabetStream: [String!]
+        deferFastField: String!
+        deferSlowField(waitFor: Int! = 5000): String!
+    }
+    type Subscription {
+        countdown(from: Int!): Int!
+    }
+`
+
+export const resolvers = {
+  Query: {
+    hello: () => 'Hello Deno!',
+    throwErrorGraphql,
+    throwErrorMasked,
+    // alphabetStream,
+    deferFastField,
+    deferSlowField,
+  },
+  Subscription: {
+    countdown,
+  }
+}
--- yoga.ts
+++ yoga.ts
@@ -1,38 +1,12 @@
 // import {createSchema, createYoga} from 'https://cdn.skypack.dev/graphql-yoga?dts'
 import {createSchema, createYoga} from 'https://cdn.jsdelivr.net/npm/graphql-yoga/+esm'
 import {useDeferStream} from 'https://cdn.jsdelivr.net/npm/@graphql-yoga/plugin-defer-stream/+esm'
-import {countdown} from "./resolvers/countdown.resolver.ts";
-import {throwErrorGraphql, throwErrorMasked} from "./resolvers/throw-error.resolver.ts";
-import {deferFastField, deferSlowField} from "./resolvers/deferAndStream.resolver.ts";
+import {resolvers, typeDefs} from "./typedefsAndResolvers.ts";
 
 export const yoga = createYoga({
-    schema: createSchema({
-        typeDefs: /* GraphQL */ `
-            type Query {
-                hello: String!
-                throwErrorGraphql: String!
-                throwErrorMasked: String!
-                alphabetStream: [String!]
-                deferFastField: String!
-                deferSlowField(waitFor: Int! = 5000): String!
-            }
-            type Subscription {
-                countdown(from: Int!): Int!
-            }
-        `,
-    resolvers: {
-      Query: {
-        hello: () => 'Hello Deno!',
-        throwErrorGraphql,
-        throwErrorMasked,
-        // alphabetStream,
-        deferFastField,
-        deferSlowField,
-      },
-      Subscription: {
-        countdown,
-      }
-    }
-    }),
+  schema: createSchema({
+    typeDefs,
+    resolvers,
+  }),
   plugins: [useDeferStream()],
 })
