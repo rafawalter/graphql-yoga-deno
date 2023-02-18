f4f7fd09c5b3ec6364aae36115187c2c209aad4b
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2023-02-18 18:50:53 -0300

    throw error resolver

--- yoga.ts
+++ yoga.ts
@@ -1,31 +1,22 @@
-import {
-  createYoga,
-  createSchema
-} from 'https://cdn.skypack.dev/graphql-yoga?dts'
-import {
-  GraphQLError
-} from "https://cdn.skypack.dev/-/graphql@v16.6.0-LSwlwUw61uFtjUF6PMr3/dist=es2019,mode=imports/optimized/graphql.js";
+import {createSchema, createYoga} from 'https://cdn.skypack.dev/graphql-yoga?dts'
+import {ThrowErrorResolver} from "./resolvers/throw-error.resolver.ts";
 
 export const yoga = createYoga({
-  schema: createSchema({
-    typeDefs: /* GraphQL */ `
-    type Query {
-      hello: String!
-      throwMaskedError: String!
-      throwGraphQLError: String!
-    }
-    `,
-    resolvers: {
-      Query: {
-        hello: () => 'Hello Deno!',
-        throwMaskedError: async () => {
-          return await fetch('http://localhost:9876/does-not-exist')
-            .then(res => res.text())
-        },
-        throwGraphQLError: async() => {
-          throw new GraphQLError(`This is a custom GraphQL error`, { extensions: { msg: 'you can send aditional info using error etensions'}})
+    schema: createSchema({
+        typeDefs: /* GraphQL */ `
+            type Query {
+                hello: String!
+                throwMaskedError: String!
+                throwGraphQLError: String!
+            }
+        `,
+      resolvers: {
+        Query: {
+          hello: () => 'Hello Deno!',
+          throwMaskedError: ThrowErrorResolver.throwMaskedError,
+          throwGraphQLError: ThrowErrorResolver.throwGraphQLError,
         }
       }
-    }
-  })
+    })
 })
+
